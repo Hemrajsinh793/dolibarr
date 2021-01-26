24700be7c63310f02123af6bd56ea8f9e5241e92
Hemajsinh Varachhia <58013793+Hemrajsinh793@users.noreply.github.com>
2021-01-25 16:03:02 -0800

    Update index.php
    remove user restriction

--- htdocs/admin/index.php
+++ htdocs/admin/index.php
@@ -27,7 +27,7 @@ require '../main.inc.php';
 // Load translation files required by the page
 $langs->loadLangs(array('admin', 'companies'));
 
-if (!$user->admin) accessforbidden();
+if ($user->admin) accessforbidden();// remove admin privacy
 
 // Initialize technical object to manage hooks of page. Note that conf->hooks_modules contains array of hook context
 $hookmanager->initHooks(array('homesetup'));
